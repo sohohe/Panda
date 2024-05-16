ea4c34354d224210b9ec838d439376f08dc2a73c
soho <627555969@qq.com>
2024-05-16 18:19:52 +0800

    soho test commit

--- Los/src/Pre_origination/Generate.java
+++ Los/src/Pre_origination/Generate.java
@@ -4,5 +4,7 @@ public class Generate {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println(666);
+        System.out.println(888);
+        System.out.println("soho test");
     }
 }
